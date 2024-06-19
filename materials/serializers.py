14ad9e7a9d60da9e3e320d6acd0e857ff6f99ab3
PleshkovaAnastasia1992 <nastya.tsvetkova@rambler.ru>
2024-06-19 15:15:06 +0500

    Add dz_24.2 zadanie_1

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,6 +1,13 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
+
+
+class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
 
 
 class CourseSerializer(ModelSerializer):
@@ -9,7 +16,12 @@ class CourseSerializer(ModelSerializer):
         fields = '__all__'
 
 
-class LessonSerializer(ModelSerializer):
+class CourseDetailSerializer(ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
+    def get_lesson_count(self, course):
+        return Lesson.objects.filter(course=course).count()
+
     class Meta:
-        model = Lesson
-        fields = '__all__'
+        model = Course
+        fields = ('id', 'name', 'description', 'lesson_count',)
